4c12896226d40e003eae26cee16553cbd805fe91
Chingari <cpatel10>
2016-12-11 17:09:02 -0600

    add address DONE Previous address DONE

--- customerDetails.html.php
+++ customerDetails.html.php
@@ -5,33 +5,100 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../CIAinn/main.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script type="text/javascript" src="../CIAinn/main.js"></script>
+<!--    <script type="text/javascript" src="../CIAinn/main.js"></script>-->
     <title>profile</title>
 </head>
   </head>
   <body>
-  <header>
+ <header>
 
-    <h1><a href="index.html">CIAinn</a></h1>
+    <h1><a href="index.php">CIAinn</a></h1>
   </header>
   
-  <nav id=profileHeader>
-	<a href="">Personal Details</a> |
-	<a href="">Address</a> |
-	<a href="">Creditcard Details</a> |
-	<a href="">Previous Reservations</a> 
-  </nav>
-  <section id=pd>
+  <div id="funtions">
+	<a class="show" target="1">Personal Details</a> |
+	<a class="show" target="2">Address</a> |
+	<a class="show" target="3">Creditcard Details</a> |
+	<a class="show" target="4">Previous Reservations</a>
+  </div>
+
+
+  <div id="fun1" class="custfun">
   <h3>Personal Details </h3>
-  </section>
-  <section id=address>
+
+
+
+
+  </div>
+  <div id="fun2" class="custfun">
+
   <h3>Address </h3>
-  </section>
-  <section id=cc>
+
+
+         <table >
+    <?php foreach ($loadAddress as $add): ?>
+
+    <tr>
+      <td> <?php echo $add['addressline1']; ?> </td>
+       <td><?php echo $add['addressline2']; ?> </td>
+	   <td> <?php echo $add['city']; ?> </td>
+	   <td> <?php echo $add['state']; ?> </td>
+        <td> <?php echo $add['zipcode']; ?> </td>
+
+       </tr>
+    <?php endforeach; ?>
+       </table>
+
+    <h3> Add Address</h3>
+  <form id="addressDetails" action="?" method="post">
+    <label for="addressline1">Address Line 1 : </label>
+      <input id="addressline1" type="text" name="addressline1" required><br><br>
+    <label for="addressline2">Address Line 2 : </label>
+    <input id="addressline2" type="text" name="addressline2" ><br><br>
+    <label for="city">City : </label>
+    <input id="city" type="text" name="city" required><br><br>
+    <label for="state">State : </label>
+    <input id="state" type="text" name="state" required><br><br>
+    <label for="zipcode">Zipcode : </label>
+    <input id="zipcode" type="text" name="zipcode" required><br><br>
+    <div><input type="submit" value="Add Address" style="float: left;"></div>
+  </form>
+  </div>
+
+
+  <div id="fun3" class="custfun">
   <h3>Creditcard Details </h3>
-  </section>
-  <section id=pr>
-  <h3>Previous Reservations </h3></section>
+    <form id="ccDetails" action="?" method="post">
+      <label for="cardNo">Card Number : </label>
+      <input id="cardNo" type="number" name="cardNo" required><br><br>
+      <label for="cardHolder">Name on Card : </label>
+      <input id="cardHolder" type="text" name="cardHolder" required ><br><br>
+      <label for="cvv">CVV : </label>
+      <input id="cvv" type="number" name="cvv" required><br><br>
+      <label for="expireMM">Expiry Month : </label>
+      <input id="expireMM" type="number" name="expireMM" required max="12" min="1"><br><br>
+      <label for="expireYY">Expiry Year : </label>
+      <input id="expireYY" type="number" name="expireYY" required min="2016"><br><br>
+      <div><input type="submit" value="Add" style="float: left;"></div>
+    </form>
+
+  </div>
+  <div id="fun4" class="custfun">
+  <h3>Previous Reservations </h3></div>
+
+  <script>
+    $('.custfun').hide();
+    $('.show').click(function () {
+      $('.custfun').hide();
+      $('#fun' + $(this).attr('target')).show();
+    });
+
+    $('.hide').click(function () {
+      $('.custfun').hide();
+
+    });
+
+  </script>
   
   <div id="footer" style="text-align: center">
     <p>©2016 CIA Inn, Inc. All rights reserved.</p>
--- customerProfile.php
+++ customerProfile.php
@@ -2,7 +2,99 @@
 
 		include_once $_SERVER['DOCUMENT_ROOT'] . '/CIAinn/includes/db.inc.php';
         include_once $_SERVER['DOCUMENT_ROOT'] . '/CIAinn/includes/helpers.inc.php';
-		
+
+$email = $_COOKIE['username'];
+debug_to_console('user email: ' . $email);
+
+try {
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/CIAinn/includes/db.inc.php';
+
+    $sql = "SELECT customerID FROM customer WHERE email = '$email'";
+    $result = $pdo->query($sql);
+    $row = $result->fetch();
+    $customerId = $row['customerID'];
+} catch (PDOException $e) {
+    $error = 'Error retrieving user information: ' . $e->getMessage();
+    include 'error.html.php';
+    exit();
+}
+
+debug_to_console('customer id: ' . $customerId);
+
+if (isset($_POST['addressline1'])){
+
+    try{
+
+    $addressline1 = $_POST['addressline1'];
+    $addressline2 = $_POST['addressline2'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zipcode = $_POST['zipcode'];
+
+    $sqlAdd = 'INSERT INTO address SET
+				customerID= :customerID,
+				addressline1= :addressline1,
+				addressline2= :addressline2,
+        		city = :city,
+        		state = :state,
+        		zipcode = :zipcode';
+
+
+
+    $sadd = $pdo->prepare($sqlAdd);
+    $sadd->bindValue(':customerID', $customerId);
+    $sadd->bindValue(':addressline1', $addressline1);
+    $sadd->bindValue(':addressline2', $addressline2);
+    $sadd->bindValue(':city', $city);
+    $sadd->bindValue(':state', $state);
+    $sadd->bindValue(':zipcode', $zipcode);
+
+    $sadd->execute();
+}
+catch (PDOException $e)
+{
+    $error = 'Error adding address: ' . $e->getMessage();
+    include 'error.html.php';
+    exit();
+}
+    echo "<script>alert('Address was added Successfully!!')</script>";
+//    header('Location:customerProfile.php');
+   echo "<script>setTimeout(\"location.href = 'customerProfile.php';\",2000);</script>";
+}
+
+try{
+
+    $sqlLoadAddress="SELECT addressline1, addressline2, city, state, zipcode FROM address WHERE customerID='$customerId' ";
+
+    $loadAddress=$pdo ->query($sqlLoadAddress);
+
+} catch (PDOException $e){
+    $error = 'Error fetching address: ' . $e->getMessage();
+    include 'error.html.php';
+    exit();
+
+}
+
+
+//if (isset($_POST['cardNumber'])) {
+//    debug_to_console('post credit card is set');
+//
+//    $cardNumber = $_POST['cardNumber'];
+//    $cardHolderName = $_POST['cardHolderName'];
+//    $cvv = $_POST['cvv'];
+//    $expireMM = $_POST['expireMM'];
+//    $expireYY = $_POST['expireYY'];
+//
+//
+//
+//
+//
+//
+//}
+
+
 		include 'customerDetails.html.php';
+
+
 		
 ?>
